b9ba9d35b0f4909ff83fb538a275f0bf90163dbc
aesquivel94 <a.esquivel@cgiar.org>
2017-04-27 09:57:15 -0500

    CCA_maps <- correccion de la funcion para que pueda correr automaticamente para todos los departamentos de interes y los trimestres.

--- forescast_model/CPT_evaluation/requerimientos_gremios/requerimientos_linux.R
+++ forescast_model/CPT_evaluation/requerimientos_gremios/requerimientos_linux.R
@@ -36,14 +36,13 @@ final_year<-2013
 
 
 ## Ruta principal donde se encuentran las carpetas con los archivos  
-ruta <- paste(getwd(), "/" ,sep="")
+ruta <- "C:/Users/AESQUIVEL/Desktop/CPT_Linux/cpt_requerimientos/"
 
 
 
-ruta_l<-paste(getwd(), "/" ,sep="")
+ruta_l<-"C:/Users/AESQUIVEL/Desktop/CPT_Linux/cpt_requerimientos/"
 ### Lectura del shp
 colombia<-shapefile(paste(ruta_l,"/colombia/colombia_depts.shp",sep=""))
-
 shp<-shapefile(paste(ruta_l,"/shp/mapa_mundi.shp",sep=""))
 
 
@@ -224,7 +223,7 @@ data_trim=function(Estaciones_C, a){ #Los argumentos son el conjunto de las esta
 # a = mes de inicio del trimestre, b = segundo mes del trimestre, c = tercer mes del trimestre
 # length_periodo = Longitud del periodo de entrenamiento
 ## cca_Maps función que realiza el gráfico cca_map de CPT, las imagenes se almacenan en la ruta
-cca_maps<-function(var_ocanoAt, yserie, Estaciones_C, xserie, lead, ruta,  a, xmin, xmax, ymin, ymax, estaciones_in, prec){
+cca_maps<-function(var_ocanoAt, names_file, yserie, Estaciones_C, xserie, lead, ruta,  a, xmin, xmax, ymin, ymax, estaciones_in, prec){
   
   ocean=which(!is.na(var_ocanoAt[[1]][])) # tome las posiciones en las que la variable sea diferente de NA
   correl=array(NA,length(ocean)) # relice un arreglo del tamaño de oceano 
@@ -346,7 +345,7 @@ cca_maps<-function(var_ocanoAt, yserie, Estaciones_C, xserie, lead, ruta,  a, xm
   modos <- modos  +   scale_x_continuous(breaks = seq(1982,final_year,3))
   
   #  Guarde los cca_maps de correlaciones
-  tiff(paste(ruta,"results/",dep,"/cca_maps_",names_file,".tif",sep=""), height=300,width=1600,res=100,
+  tiff(paste(ruta,"/cca_maps_",names_file,".tif",sep=""), height=300,width=1600,res=100,
        compression="lzw") # height=1280, width=2048, pointsize=2, res=200,
   grid.arrange(Map_x,modos,p,ncol=5, layout_matrix = rbind(c(1,1,2,3,3)))
   dev.off()
@@ -358,21 +357,9 @@ cca_maps<-function(var_ocanoAt, yserie, Estaciones_C, xserie, lead, ruta,  a, xm
 ruta_c <- "C:/Users/AESQUIVEL/Desktop/CPT_Linux/cpt_requerimientos/Cross_validated_15_5_10"
 
 
-# "casanare"    "cordoba"    "tolima"    "valle"  "santander"
-dep= "santander" # variar el departamento
-
-lead<- c("DEF_Nov", "DEF_Aug", "DEF_Jun")
-a<- rep(12,3)
-
-#### Declaración de las constantes
-length_periodo<- rep(31,3)  # Ancho del periodo de estudio para cada trimestre
-
-
-cbind.data.frame(a, lead, length_periodo)
-
-
+maps_dep<-function(dep, a, lead, length_periodo){
 # Lectura d eas estaciones para cada departamento.
-Estaciones_C <- read.delim(paste(ruta,"/dep/precip_",dep,".txt",sep=""),skip =3, header=T, sep="")
+Estaciones_C <- read.delim(paste(ruta_l,"dep/precip_",dep,".txt",sep=""),skip =3, header=T, sep="")
 # Determinación de los limites departamentales y estaciones a dibular en el cap >.<
 if(dep=="casanare"){
   xmin<- -73.5; xmax<- -71; ymin<-  4; ymax<-  6
@@ -392,7 +379,6 @@ if(dep=="casanare"){
 }
 
 #############
-
 if(prec=="SST"){
  
   ##### Si la region es optimizada correr este for
@@ -410,7 +396,7 @@ if(prec=="SST"){
     yserie <- read.csv(paste(ruta_c,"/Y_CCA_Map_Series_",a[i],"_",lead[i],"_precip_",dep,".txt",sep=""),skip =2, header=T, sep="")
     names_file <- paste(a[i],"_",lead[i],"_", dep,sep="")
     
-    SST<-read.table(paste(ruta,"/SST/",lead[i],".tsv",sep=""),sep="\t",dec=".",skip =2,fill=TRUE,na.strings =-999)
+    SST<-read.table(paste(ruta,"/",prec,"/",lead[i],".tsv",sep=""),sep="\t",dec=".",skip =2,fill=TRUE,na.strings =-999)
     ## Conversión a raster
     SST<-rasterize(SST)
     
@@ -418,9 +404,10 @@ if(prec=="SST"){
     b<-extent(lista[[i]][,3])
     var_ocanoAt=crop(var_ocanoAt, b)
     
+    rutas_d<-paste(ruta, "results/", dep, sep = "")
     
-    cca_maps(var_ocanoAt, yserie, Estaciones_C, xserie, lead[i], ruta,  a[i], xmin, xmax, ymin, ymax, estaciones_in, prec)
-    print(i)
+    cca_maps(var_ocanoAt,names_file, yserie, Estaciones_C, xserie, lead[i], ruta = rutas_d,  a[i], xmin, xmax, ymin, ymax, estaciones_in, prec)
+    print(paste("prueba piloto",i, sep=" "))
   }
 }else if(prec!="SST"){
   
@@ -441,16 +428,141 @@ if(prec=="SST"){
     yserie <- read.csv(paste(ruta_c,"/Y_CCA_Map_Series_",a[i],"_",lead[i],"_precip_",dep,".txt",sep=""),skip =2, header=T, sep="")
     names_file <- paste(a[i],"_",lead[i],"_", dep,sep="")
     
-    SST<-read.table(paste(ruta,"/rhum_700/", lead[i],".tsv", sep=""),sep="\t",dec=".",skip =2,fill=TRUE,na.strings =-999)
+    
+    SST<-read.table(paste(ruta,"/",prec,"/", lead[i],".tsv", sep=""),sep="\t",dec=".",skip =2,fill=TRUE,na.strings =-999)
     ## Conversión a raster
     SST<-rasterize(SST, prec)
     var_ocanoAt <-SST[[1:length_periodo[i]]]
     b<-extent(lista[[i]][,3])
     var_ocanoAt<-crop(rotate(var_ocanoAt), b)
     
-    cca_maps(var_ocanoAt, yserie, Estaciones_C, xserie, lead[i], ruta,  a[i], xmin, xmax, ymin, ymax, estaciones_in, prec)
+    
+    rutas_d<-paste(ruta, "results/", dep, sep = "")
+    
+    cca_maps(var_ocanoAt,names_file, yserie, Estaciones_C, xserie, lead[i], ruta = rutas_d,  a[i], xmin, xmax, ymin, ymax, estaciones_in, prec)
+    
     print(paste("prueba piloto",i, sep=" "))
   }
 }
+}
 
 
+lead<- c("DEF_Nov", "DEF_Aug", "DEF_Jun")
+a<- rep(12,3)
+
+#### Declaración de las constantes
+length_periodo<- rep(31,3)  # Ancho del periodo de estudio para cada trimestre
+cbind.data.frame(a, lead, length_periodo)
+
+dep<-"valle"
+maps_dep(dep,a,lead,length_periodo)
+
+
+### Para todas las regiones en caso que tenga 
+dep<-list("casanare", "cordoba", "santander", "tolima", "valle")
+sapply(dep, maps_dep, a, lead, length_periodo, simplify = T)
+
+
+
+
+
+###########################################################
+###########################################################
+###########################################################
+
+## Esta función devuelve las gráficas del goodness index, 
+## ruta_c ruta donde se encuentran los archivos
+## dep_f son los departamentos que se van a gráficar
+GoodnessIndex <- function(ruta_c,dep_f){
+  #### Lead times para cada trimestre
+  lead<-c(paste(rep("MAM",3),c("Feb","Nov","Sep"), sep="_"),
+          paste(rep("JJA",3),c("May","Feb","Dec"), sep="_"),
+          paste(rep("SON",3),c("Aug","May","Mar"), sep="_"),
+          paste(rep("DEF",3),c("Nov","Aug","Jun"), sep="_"))
+  ### Trimestres
+  a<- c(rep(3,3),rep(6,3),rep(9,3),rep(12,3))
+  
+  good_index=0 # inicialice el vector
+  GoodnessIndex=NA # inicialice el data frame
+  for(j in 1:length(dep_f)) # realice esto para todos los departamentos
+    for(i in 1:12){ # barra para todos los periodos con sus respectivos lead time
+      # Lea la tabla 
+      goodnex<-read.table(paste(ruta_c,"GoodnessIndex_",a[i],"_",lead[i],"_precip_",dep_f[j],".txt", sep=""),  skip=6, colClasses=c(rep("numeric",3),"character",rep("numeric",4)))
+      # Cambie los nombres del archivo 
+      names(goodnex)<-c("x1","y1","cca1","index1", "x2", "y2","cca2", "index2")
+      # Encuentre el maximo
+      goodnex$index2[dim(goodnex)[1]]==max(goodnex$index2)
+      # Extraiga solo la fila donde estara el maximo y adicione
+      # las columnas departamento, mes de inicio trimestre y lead time
+      good_index<-cbind(dep_f[j], a[i] ,lead[i],goodnex[dim(goodnex)[1],5:8])
+      # Almacene la fila en un data frame
+      GoodnessIndex<-rbind(GoodnessIndex,good_index)
+    }
+  # Quite la primera fila ya que es nula
+  GoodnessIndex<-GoodnessIndex[-1,]
+  # Quite los nombres de las filas
+  rownames(GoodnessIndex)=NULL
+  # Cambie los nombres de las columnas
+  names(GoodnessIndex)=c("dep","a", "lead", "modos_x", "modos_y", "modos_cca", "GoodnessIndex")
+  GoodnessIndex <- cbind(GoodnessIndex,lead_time=rep(rep(c(0,3,5),4),length(dep_f))  )
+  
+  
+  
+  setwd(paste("C:/Users/AESQUIVEL/Google Drive/new_predictor/Exp2/", region,"/", prec, "/results/", sep=""))
+  ### Guarde todos los Goodness Index en un archivo .csv
+  write.csv(x = GoodnessIndex, file = "GoodnessIndex.csv",sep = ",")
+  
+  
+  #setwd("C:/Users/AESQUIVEL/Google Drive/new_predictor/Exp2/results_graphs_C/", prec,"/results")
+  
+  GoodnessIndex$a[GoodnessIndex$a==12]=0 # Cambiarle el número para que diciembre 
+  #aparezca primero
+  # Realice el gráfico de cajas para todos los departamentos.
+  for(i in 1:length(dep_f)){
+    dep_inf<-  GoodnessIndex[GoodnessIndex$dep==dep_f[i],]
+    to_string  <- as_labeller(c(`0`="DEF",`3` = "MAM", `6` = "JJA", `9` = "SON"))
+    graph_dep  <- ggplot(dep_inf, aes(x = factor(lead_time), y = GoodnessIndex)) 
+    graph_dep  <- graph_dep  + geom_bar(stat = 'identity', fill="gray60") + ylim(-0.06,0.5)
+    graph_dep  <- graph_dep  + facet_wrap(~a, nrow = 1, labeller = to_string )
+    graph_dep  <- graph_dep  + scale_x_discrete(breaks=c(0, 3, 5),
+                                                labels=c("LT-0", "LT-3", "LT-5")) +  labs(x="Lead Time", y=paste("Goodness Index", sep=""))
+    graph_dep  <-graph_dep  + theme_bw() + theme(legend.position = "none") 
+    
+    
+    tiff(paste(ruta,"/results/",dep_f[i],"/GoodnessIndex_dep_",dep_f[i],".tif",sep=""), height=720,width=1280,res=200,
+         pointsize=2,compression="lzw")
+    print(graph_dep)
+    dev.off()
+  }
+  
+  
+  
+  ### Grafico de Linea puede hacerse solopara el lead simultaneo o para los maximos
+  ### modificando la función 
+  Sim=GoodnessIndex[GoodnessIndex$lead_time==0,]
+  names(Sim)[1]="Departamento"
+  levels(Sim$Departamento)<-c("Casanare", "Cordoba", "Tolima",  "Valle", "Santander")
+  graph_line  <- ggplot(Sim, aes(x =a, y = GoodnessIndex, color=Departamento))
+  graph_line  <- graph_line + geom_line(aes(linetype=Departamento), size=1) + ylim(-0.06,0.5) #ylim(-0.05,0.5)
+  graph_line  <- graph_line + geom_point(aes(shape=Departamento), size=2)
+  graph_line  <- graph_line + theme_bw()  + labs(x="", y="Goodness Index") 
+  graph_line  <- graph_line +  scale_x_continuous(breaks = c(0,3,6,9), labels = c("DEF","MAM", "JJA", "SON"))
+  graph_line  <- graph_line + theme(legend.title = element_text(size = 10.5),
+                                    legend.key.height=unit(0.5,"cm"),
+                                    legend.key.width=unit(0.8,"cm"))
+  
+  
+  # Guarde el graph
+  tiff(paste(ruta,"/results/GoodnessIndex_line.tif",sep=""), height=720,width=1280,res=200,
+       pointsize=2,compression="lzw")
+  print(graph_line)
+  dev.off()
+  return(GoodnessIndex)}
+
+
+
+
+## Corrida para todos los archivos
+ruta_c
+dep_f<-c("casanare",    "cordoba",    "tolima",    "valle", "santander")
+table<-GoodnessIndex(ruta_c = ruta_c, dep_f = dep_f)
